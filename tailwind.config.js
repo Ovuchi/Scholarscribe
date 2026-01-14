53ede3bd80f6522aa4b93a0e7feb2b20534847de
Ovuchi Ovunda <ovuchiovunda12@gmail.com>
2026-01-14 22:23:02 +0100

    Complete hero section and add project assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./pages/**/*.html", "./js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "scholar-heading": "#0077B6",
+        "scholar-body": "#334155",
+      },
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+        heading: ["Plus Jakarta Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
